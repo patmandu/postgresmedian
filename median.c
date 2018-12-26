2c483e48643ab0fb0f79cdf87eeb6311a4aff1a9
patman <patman@REDDWARF>
2018-12-25 23:14:53 -0500

    Rewritten to use tuplesort routines.  Mostly working, but needs more polish.

--- median.c
+++ median.c
@@ -1,9 +1,15 @@
 #include <postgres.h>
 #include <fmgr.h>
+#include <utils/tuplesort.h>
+#include <catalog/namespace.h>
+#include <nodes/parsenodes.h>
+
 
 #ifdef PG_MODULE_MAGIC
 PG_MODULE_MAGIC;
 #endif
+
+#define MEDIANDEBUG DEBUG1
 
 /*
 Pat Mancuso - Timescale homework project 12/16/2018
@@ -13,9 +19,8 @@ Find the median value
 PG_FUNCTION_INFO_V1(median_transfn);
 
 typedef struct {
-  unsigned int values_size;  /* Allocated size of values array */
-  unsigned int values_num;   /* Number of active entries in the values array */
-  Datum * values;
+  int64 values_num;   /* Number of entries */
+  Tuplesortstate *tuplesort_state;
 } MEDIAN_STATE;
 
 #define VALUES_SIZE_START 16
@@ -35,74 +40,59 @@ median_transfn(PG_FUNCTION_ARGS)
 
     if (!AggCheckCallContext(fcinfo, &agg_context)) {
       elog(ERROR, "median_transfn called in non-aggregate context");
-      PG_RETURN_NULL();
     }
-    else
+
     {
       MEDIAN_STATE *state = (MEDIAN_STATE*) PG_GETARG_POINTER(0);
+      MemoryContext old_context = MemoryContextSwitchTo(agg_context);
 
       if (!state) {
-          /* If state is null, this is first call, which means we should create the state */
-          state = palloc(sizeof(*state));
-          if (!state) {
-             elog(ERROR, "median_transfn failed to alloc state");
-             PG_RETURN_NULL();
-          }
-          state->values_size = VALUES_SIZE_START;
-          state->values_num = 0;
-          state->values = (Datum*)(palloc(sizeof(Datum) * state->values_size));
-          if (!state->values) {
-             elog(ERROR, "median_transfn failed to alloc values array");
-             PG_RETURN_NULL();
-          }
-       }
+        /* If state is null, this is first call, which means we should create the state */
+        state = palloc(sizeof(MEDIAN_STATE));
+        if (!state) {
+          MemoryContextSwitchTo(old_context);
+          elog(ERROR, "median_transfn failed to alloc state");
+        }
 
-       if (state->values_size < state->values_num) {
-         /* A Very Bad Thing has happened, should never have a number of values stored bigger than the size of the array */
-         elog(ERROR, "median_transfn invalid index in values array");
-         PG_RETURN_NULL();
-       }
+        state->values_num = 0;
 
-       if (state->values_size == state->values_num) {
-         /* At max size, need to realloc the values array to make room for new value, double the previous size */
-         unsigned int new_values_size = state->values_size * 2;
+        {
+          Oid datumType = get_fn_expr_argtype(fcinfo->flinfo, 1);
+          Oid sortOperator = 0;
+          Oid sortCollation = fcinfo->fncollation;
+          bool nullsFirstFlag = false;
+          int workMem = 65536;  /* wild guess - may need to be tuned */
+          bool randomAccess = false;
+          char *oprname = "<";
 
-         if (new_values_size < state->values_size) {
-           /* If doubling the size results in a smaller number, the size has overflowed */
+          /* Get the sort operator oid (ref: namespace.c) */
+          sortOperator = OpernameGetOprid(list_make1(makeString(oprname)), datumType, datumType);
 
-           /* one more try...set it to the max */
-           new_values_size = MAX_UNSIGNED_INT;
+          /* Initialize the tuplesort state */
+          state->tuplesort_state = tuplesort_begin_datum(datumType, sortOperator, sortCollation, nullsFirstFlag, workMem, randomAccess);
+        }
 
-           if (new_values_size == MAX_UNSIGNED_INT) {
-             /* if we're already at the max...fail gracefully */
-             elog(ERROR, "median_transfn exceeded max size");
-             PG_RETURN_NULL();
-           }
-         }
-
-         /* Try to allocate array to new_values_size length */
-         {
-           Datum *new_values = (Datum*)(palloc(sizeof(Datum) * new_values_size));
-           Datum *old_values = state->values;
-         
-           if (!new_values) {
-             elog(ERROR, "median_transfn failed to alloc values array");
-             PG_RETURN_NULL();
-           }
-           /* copy the old array data to the new array and free the old array */
-           memcpy(new_values, old_values, state->values_size * sizeof(Datum));
-           state->values = new_values;
-           state->values_size = new_values_size;
-           pfree(old_values);
-         }
+        if (!state->tuplesort_state) {
+          MemoryContextSwitchTo(old_context);
+          elog(ERROR, "median_transfn failed to alloc tuplesort_state");
+        }
       }
 
-      /* Now that we've got a state, and the values array is big enough to accept the data...store it */
+      /* Now that we've got a state, and it's ready to accept the data...store it...if it's not null */
+      if (PG_ARGISNULL(1))
+      {
+        elog(MEDIANDEBUG, "median_transfn ignoring null arg");
+      }
+      else
       {
         Datum value = PG_GETARG_DATUM(1);
-        state->values[state->values_num++] = value;
+        elog(MEDIANDEBUG, "median_transfn about to add entry");
+        tuplesort_putdatum(state->tuplesort_state, value, PG_ARGISNULL(1));
+        state->values_num++;
+        elog(MEDIANDEBUG, "median_transfn added entry %lld", state->values_num);
       }
 
+      MemoryContextSwitchTo(old_context);
       PG_RETURN_POINTER(state);
     }
 }
@@ -120,52 +110,82 @@ Datum
 median_finalfn(PG_FUNCTION_ARGS)
 {
     MemoryContext agg_context;
-    Datum result = 0;
 
     if (!AggCheckCallContext(fcinfo, &agg_context)) {
       elog(ERROR, "median_finalfn called in non-aggregate context");
-      PG_RETURN_NULL();
     }
 
     {
-    MEDIAN_STATE *state = (MEDIAN_STATE*) PG_GETARG_POINTER(0);
+      MemoryContext old_context = MemoryContextSwitchTo(agg_context);
+      MEDIAN_STATE *state = (MEDIAN_STATE*) PG_GETARG_POINTER(0);
+      bool valid_result = false;
+      Datum result = {0};
+  
+      elog(MEDIANDEBUG, "median_finalfn about to retrieve state");
 
-    if (!state) {
-      /* A Bad Thing has happened, state should not be null here */
-      elog(ERROR, "median_finalfn can not retrieve state");
-      PG_RETURN_NULL();
-    }
+      if (!state) {
+        /* State will be null if the table was empty...all done */
+        elog(MEDIANDEBUG, "median_finalfn can not retrieve state");
+        MemoryContextSwitchTo(old_context);
+        PG_RETURN_NULL();
+      }
+  
+      {
+        /* cheat a bit here, use the mid value even if it's an even number of total values to avoid trying to average non-numeric values */
+        int64 midpoint = (state->values_num+1)/2;
 
-    if (state->values_num == 0) {
-      /* No data to operate on */
-      elog(ERROR, "median_finalfn no data to operate on");
-      if (state->values)
-        pfree(state->values);
+        elog(MEDIANDEBUG, "median_finalfn values_num=%lld midpoint=%lld", state->values_num, midpoint);
+
+        /* do the sort */
+        elog(MEDIANDEBUG, "median_finalfn about to do sort");
+        tuplesort_performsort(state->tuplesort_state);
+
+        elog(MEDIANDEBUG, "median_finalfn about to skip to result");
+        valid_result = tuplesort_skiptuples(state->tuplesort_state, midpoint-1, true);
+        if (!valid_result) {
+          MemoryContextSwitchTo(old_context);
+          elog(ERROR, "median_finalfn can not skip tuples");
+        }
+        else
+        {
+          bool forward = true;
+          bool isNull = false;
+          Datum abbrev = {0};
+
+          elog(MEDIANDEBUG, "median_finalfn about to retrieve result");
+          valid_result = tuplesort_getdatum(state->tuplesort_state, forward, &result, &isNull, &abbrev);
+          if (!valid_result) {
+            MemoryContextSwitchTo(old_context);
+            elog(ERROR, "median_finalfn can not get result datum");
+          }
+          else
+          {
+            elog(MEDIANDEBUG, "median_finalfn retrieved result");
+          }
+        }
+      }
+  
+      /* Free the state here */
+      if (state && state->tuplesort_state)
+      {
+        elog(MEDIANDEBUG, "median_finalfn about to end tuplesort state");
+        tuplesort_end(state->tuplesort_state);
+      }
       if (state)
+      {
+        elog(MEDIANDEBUG, "median_finalfn about to free state");
         pfree(state);
-      PG_RETURN_NULL();
+      }
+  
+      MemoryContextSwitchTo(old_context);
+      if (valid_result) {
+        elog(MEDIANDEBUG, "median_finalfn returning valid result");
+        PG_RETURN_DATUM(result);
+      }
+      else
+      {
+        elog(MEDIANDEBUG, "median_finalfn no valid result, returning null");
+        PG_RETURN_NULL();
+      }
     }
-
-    /* TODO: sort the data */
-
-    if (state->values_num % 2) {
-      /* median is the middle value if there are an odd number of values... */
-      result = state->values[state->values_num/2];
-    }
-    else
-    {
-      /* ...or the average of the two values at the middle if there are an odd number of values */
-      /* TODO: do the math in terms of Datum */
-      result = (state->values[state->values_num/2] + state->values[(state->values_num/2)+1]) / 2;
-    }
-
-    /* Free the state here */
-    if (state->values)
-      pfree(state->values);
-    if (state)
-      pfree(state);
-
-    }
-
-    PG_RETURN_DATUM(result);
-}
+  }
